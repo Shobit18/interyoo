d8ba7fa804fd25f190053a54a7eb4ed0f409ce51
shobit kunwar <shobitkunwar2018@gmail.com>
2022-03-21 17:54:33 +0530

    new change

--- src/views/ClosedTab.vue
+++ src/views/ClosedTab.vue
@@ -1,12 +1,14 @@
 <template>
   <div>
-    <h1 class="font-bold mx-10 my-4 text-xl">Position </h1>
+
+
+    <!-- <h1 class="font-bold mx-10 my-4 text-xl">Position </h1>
     <div class="flex m-4 py-2 border-2 space-x-5 mx-20 w-44 justify-center ">
       <a class="flex  mx-2 text-white text-slate-500 hover:bg-blue-600 rounded"><router-link to ="./OpenTab.vue">Open</router-link></a>
       <a class="flex bg-black mx-2 text-white text-slate-500 hover:bg-blue-600 rounded"><router-link to ="./ClosedTab.vue">Close</router-link></a>
-    </div>
+    </div> -->
 
-    <div class="border-2 mx-20 py-4 hover:bg-blue-200 rounded ">
+    <div class="border-2 mx-20 py-4 hover:bg-blue-200 rounded  m-4">
       <h3 class="mx-4 item-center font-bold ">Vue.js Developer </h3>
       <div class="flex justify-between ">
       <div class="flex space-x-3 mr-3">
@@ -29,6 +31,9 @@
 <script>
 export default {
   name: 'ClosedTab',
+  data() {
+    
+  }
 }
 </script>
 
