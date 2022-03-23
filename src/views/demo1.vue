f74c29e91388625b9199781f8bb2f8537d5ce1d5
shobit kunwar <shobitkunwar2018@gmail.com>
2022-03-23 18:34:16 +0530

    new change

---
+++ src/views/demo1.vue
@@ -0,0 +1,38 @@
+import { ref, reactive, toRefs, computed } from "vue"
+const myArray = [];
+
+
+
+
+ 
+  // const state = reactive(
+  //   {
+  //     myArray: []
+      
+  //   }
+  // )
+  // const arrayObj = computed(function() {
+  //   return `${state.myArray}`
+  // })
+  
+
+  
+  //   return {
+  //       addPosition: 'Add Position',      
+  //      myArr:myArray,
+      
+  // },
+
+function createNewPosition() {
+        let obj1 = {
+          title:"(Change the name of this title)"
+        };
+        myArray.push(obj1);
+    console.log(myArray);
+
+       return {
+        addPosition: 'Add Position',      
+       myArr:myArray,
+      
+  }
+      } 
